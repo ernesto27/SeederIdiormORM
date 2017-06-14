febdf448036283711111209bef51636f389302ba
ernesto ponce <ernesto@pigmalionstudios.com.ar>
2017-06-14 18:05:56 -0300

    Agrego tests seteo nombre de tabla , data dummy

--- src/ernesto27/seeder/Seeder.php
+++ src/ernesto27/seeder/Seeder.php
@@ -11,7 +11,20 @@ class Seeder
     private static $instance = null;
 
     /**
+     * Nombre de la tabla DB
+     * @var object
+     */
+    private $tableName;
+
+    /**
+     * Data dummy que se guarda en el modelo
+     * @var array
+     */
+    private $data;
+
+    /**
      * Crea una instancia de la clase utilizand un singleton
+     * @return object
      */
     public static function init()
     {
@@ -21,5 +34,35 @@ class Seeder
         return self::$instance;
     }
 
+    /**
+     * Setea el nombre de la tabla
+     * @param string
+     * @return
+     */
+    public function table($value)
+    {
+        $this->tableName = $value;
+        return $this;
+    }
+
+    /**
+     * Setea la data dummy que se guarda en DB
+     * @param array
+     * @return
+     */
+    public function data($data)
+    {
+        $this->data = $data;
+    }
+
+    public function getTableName()
+    {
+        return $this->tableName;
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
 
 }
--- tests/SeederTest.php
+++ tests/SeederTest.php
@@ -13,14 +13,31 @@ class SeederTest extends \PHPUnit_Framework_TestCase
 	{
         // Create a model and save on DB
         // Seeder::init()->table('sometable')->data($posts)->create(5)
-
         $this->seeder = Seeder::init();
 	}
 
 	/** @test */
 	public function it_should_create_an_instance_of_seeder_class()
 	{
-        var_dump($this->seeder);
         $this->assertInstanceOf(Seeder::class, $this->seeder);
 	}
+
+    /** @test */
+    public function it_should_set_a_table_name()
+    {
+        $this->seeder->table('posts');
+        $this->assertEquals('posts', $this->seeder->getTableName());
+    }
+
+    /** @test */
+    public function it_should_set_data_dummy_array()
+    {
+        $data = array(
+            array('key' => 'title1', 'title post2'),
+            array('key' => 'title2', 'title post2')
+        );
+        $this->seeder->table('posts')->data($data);
+        $this->assertEquals($data, $this->seeder->getData());
+    }
+
 }
